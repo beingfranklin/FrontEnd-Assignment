c3d197505a2a3475fa7b79fea7a617d4bb71cfb7
Franklin Antony <franklinanton.007@gmail.com>
2018-04-30 19:53:49 +0530

    Corrected values

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 
-var name = document.getElementById('uname').value;;
+var uname = document.getElementById('uname').value;;
 var pw = document.getElementById('pw').value;;
 
 // storing input from register-form
 function store() {
     console.log("fun1")
-    localStorage.setItem('uname', uname.value);
-    localStorage.setItem('pw', pw.value);
+    localStorage.setItem('uname', uname);
+    localStorage.setItem('pwd', pw);
     alert('Registered In');
 }
 
 function ulogin(){
     var storedName = localStorage.getItem('uname');
-    var storedPw = localStorage.getItem('pw');
-    console.log("fun1")
+    var storedPw = localStorage.getItem('pwd');
+    console.log("fun2")
     // entered data from the login-form
-    var userName = document.getElementById('userName').value;
-    var userPw = document.getElementById('userPw').value;
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
     if(userName.value == storedName && userPw.value == storedPw) {
         alert('You are loged in.');
     }else {
